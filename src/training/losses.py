ff319c89de2a3c8365dba6cb7ee14a40b3ae17b6
HLF2005 <haroldlefloch@gmail.com>
2026-07-27 11:05:57 +0200

    Update losses.py

--- src/training/losses.py
+++ src/training/losses.py
@@ -16,34 +16,29 @@ class DiceCrossEntropyLoss(nn.Module):
 
     def forward(self, logits, targets):
         ce_loss = self.cross_entropy(logits, targets)
+
+        # Transform logit to probabilities
         probabilities = F.softmax(logits, dim=1)
-        targets_one_hot = F.one_hot(
-            targets,
-            num_classes=self.num_classes,
-        )
-        targets_one_hot = targets_one_hot.permute(
-            0, 3, 1, 2
-        ).float()
+
+        # One-hot encoding for the label
+        targets_one_hot = F.one_hot(targets, num_classes=self.num_classes)
+        targets_one_hot = targets_one_hot.permute( 0, 3, 1, 2).float() # dim format: [Batch, Classes, H, W].
+
+        # Skip the class background (classe 0)
         probabilities = probabilities[:, 1:]
         targets_one_hot = targets_one_hot[:, 1:]
-        dimensions = (0, 2, 3)
-        intersection = (
-            probabilities * targets_one_hot
-        ).sum(dim=dimensions)
 
-        denominator = (
-            probabilities.sum(dim=dimensions)
-            + targets_one_hot.sum(dim=dimensions)
-        )
-        dice_per_class = (
-            2 * intersection + self.smooth
-        ) / (
-            denominator + self.smooth
-        )
+        # Calcul of th Dice Loss
+        dimensions = (0, 2, 3)
+        intersection = ( probabilities * targets_one_hot).sum(dim=dimensions)
+
+        denominator = (probabilities.sum(dim=dimensions) + targets_one_hot.sum(dim=dimensions))
+
+        dice_per_class = ( 2 * intersection + self.smooth) / ( denominator + self.smooth)
+
         dice_loss = 1 - dice_per_class.mean()
-        total_loss = (
-            self.ce_weight * ce_loss
-            + self.dice_weight * dice_loss
-        )
+
+        # Total loss
+        total_loss = ( self.ce_weight * ce_loss + self.dice_weight * dice_loss)
 
         return total_loss
